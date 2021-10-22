7caf3cfa976bdccface08aa9bc00862f4bd3ed59
João Félix <joaosilva16387@gmail.com>
2021-10-22 13:32:58 -0300

    Added the performance at site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Home } from './containers'
 import { Provider } from 'react-redux'
 import store from './store'
+import 'antd/dist/antd.css';
 
 export const App = () => {
   return (
